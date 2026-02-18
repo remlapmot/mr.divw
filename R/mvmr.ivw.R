b347e10dbd6b7f7aa9ba0ac3dd31f6d26c7d1c75
Tom Palmer <remlapmot@hotmail.com>
2026-02-18 11:33:02 +0000

    Fix long lines in helpfile

--- R/mvmr.ivw.R
+++ R/mvmr.ivw.R
@@ -14,8 +14,10 @@
 #'
 #' @examples
 #' data("hdl_subfractions")
-#' # We are going to estimate the effect of S-HDL-P on the risk of CAS with adjustments of HDL, LDL, and TG levels
-#' beta.exposure <- hdl_subfractions$data[,c("gamma_exp1","gamma_exp2","gamma_exp3","gamma_exp4")] # corresponding to SNP effects on respectively  HDL, LDL, TG, and S-HDL-P
+#' # Estimate the effect of S-HDL-P on CAS risk, adjusting for HDL, LDL, and TG
+#' # Columns: SNP effects on HDL, LDL, TG, and S-HDL-P respectively
+#' beta.exposure <- hdl_subfractions$data[,
+#'   c("gamma_exp1","gamma_exp2","gamma_exp3","gamma_exp4")]
 #' se.exposure <- hdl_subfractions$data[,c("se_exp1","se_exp2","se_exp3","se_exp4")]
 #' beta.outcome <- hdl_subfractions$data$gamma_out1
 #' se.outcome <- hdl_subfractions$data$se_out1
